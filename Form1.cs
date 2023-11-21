68617979ee936cbc2a290ee2f144b56d4a26850b
Luisasdasdsad <150839706+Luisasdasdsad@users.noreply.github.com>
2023-11-21 11:47:00 -0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,30 +1,37 @@
-namespace Pantalla_de_pagar
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libreria
 {
     public partial class Form1 : Form
     {
+        const decimal PRECIO_LIBROS_1_3 = 30;
+        const decimal PRECIO_LIBROS_4_6 = 40;
         public Form1()
         {
             InitializeComponent();
         }
+        
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            tabControlGeneral.SelectedTab = tabPageInicio;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
+            tabControlPedido.SelectedTab = tabPageComprar;
         }
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -32,64 +39,38 @@ namespace Pantalla_de_pagar
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void buttonCalcular_Click(object sender, EventArgs e)
         {
+            decimal totalPagar = CalcularTotal();
 
+            // Mostrar el total en el label correspondiente
+            labelTotalPedido.Text = $"Total a Pagar: {totalPagar.ToString("C")}";
         }
-
-        private void button6_Click(object sender, EventArgs e)
+        private decimal CalcularTotal()
         {
+            decimal totalPagar = 0;
 
-        }
+            // Obtener los libros seleccionados
+            foreach (var libro in checkedListBox1.CheckedItems)
+            {
+                int indiceLibro = checkedListBox1.Items.IndexOf(libro);
 
-        private void button7_Click(object sender, EventArgs e)
-        {
+                if (indiceLibro >= 0 && indiceLibro <= 2)
+                {
+                    totalPagar += PRECIO_LIBROS_1_3;
+                }
+                else if (indiceLibro >= 3 && indiceLibro <= 5)
+                {
+                    totalPagar += PRECIO_LIBROS_4_6;
+                }
+            }
 
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox6_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void domainUpDown6_SelectedItemChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void domainUpDown6_SelectedItemChanged_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabPage12_Click(object sender, EventArgs e)
-        {
-
+            return totalPagar;
         }
     }
 }
